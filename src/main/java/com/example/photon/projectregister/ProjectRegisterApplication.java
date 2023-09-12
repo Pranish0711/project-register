43e360c96a686b49bedaf8f572ede0274ba2ea39
Pranish <113571081+Pranish0711@users.noreply.github.com>
2023-09-12 13:08:59 +0530

    Add files via upload
    beginning of project

---
+++ src/main/java/com/example/photon/projectregister/ProjectRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.example.photon.projectregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectRegisterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/photon/projectregister/ProjectRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.photon.projectregister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
